ced6d797269f58b5518b2c6ed7946f4d117c19d3
Arkam <arkam.ardsb@gmail.com>
2023-01-30 16:23:10 +0530

    Updates

---
+++ precache-manifest.2f12f9e98499f2c6923dac981b8ced67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972c2c59da0b1598a244c4dd27c5d2df",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "940288990baabfa83952",
+    "url": "/weather-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e9e1e570e88985e21e91",
+    "url": "/weather-app/static/css/main.b749b69d.chunk.css"
+  },
+  {
+    "revision": "940288990baabfa83952",
+    "url": "/weather-app/static/js/2.22260845.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.22260845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e1e570e88985e21e91",
+    "url": "/weather-app/static/js/main.d6f90b54.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
